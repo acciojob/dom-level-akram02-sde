c31a179675c48bff63617f05c96aa3d9fc3b50b1
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-08-04 23:17:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let level = document.getElementById('level');
+let c = 1;
+        
+while (level !== null && level.tagName.toLowerCase() !== 'html' ) {
+  level = level.parentElement; // Update the 'level' variable to its parent node
+  c++;     
+}   
+ 
+alert(c);           
